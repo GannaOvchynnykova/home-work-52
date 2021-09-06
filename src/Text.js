ec5f4d0539c084ee0cdd50ded8b0ca19a235967c
GannaOvchynnykova <ov4innikova.an@gmail.com>
2021-09-07 00:02:25 +0200

    react-intro

---
+++ src/Text.js
@@ -0,0 +1,7 @@
+export default function Text() {
+    return (
+        <div class='text'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa, dolore perferendis mollitia nisi dolores harum quos, accusamus, maiores dolorum quibusdam natus autem officiis. Officiis reiciendis laboriosam asperiores sunt officia quia magni voluptas incidunt ab deserunt sapiente aperiam eum recusandae repellat et at ipsum voluptatum, dolor ad dolores nam excepturi!</p>
+        </div>
+    )
+}
---
+++ src/Title.js
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+        <div class='title'>
+            <h1>Magic Button</h1>
+        </div>
+    )
+}
